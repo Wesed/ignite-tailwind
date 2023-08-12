8247103f4073aa6201cf57deaad7db005dff648c
Weslley Eduardo <dev.wfeduardo@gmail.com>
2023-08-11 21:13:16 -0300

    .

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -35,12 +35,14 @@ export function Sidebar() {
         <NavItem title="Reporting" icon={Flag} />
         <NavItem title="Users" icon={Users} />
       </nav>
+
       <div className="mt-auto flex flex-col gap-6">
         <nav className="space-y-0.5">
           <NavItem title="Support" icon={LifeBuoy} />
           <NavItem title="Settings" icon={Cog} />
         </nav>
       </div>
+
       <UsedSpaceWidget />
 
       <div className="h-px bg-zinc-200" />
